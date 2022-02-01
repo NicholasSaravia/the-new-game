f84b95608baf23e174073dd5ee9a4909baccbdd7
Nicholas Saravia <NicholasSaravia@users.noreply.github.com>
2022-01-31 21:17:05 -0500

    app colors configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'new-game-dark-blue': '#223547',
+        'new-game-light-blue': '#15659D',
+        'new-game-success-green': '#34C759',
+        'new-game-error-red': '#FF3B30'       
+      }
+    },
   },
   plugins: [],
 }
