da821e8431785db4f8c9c9fac7b07fbd7942fdfe
Nicholas Saravia <NicholasSaravia@users.noreply.github.com>
2022-02-02 21:28:30 -0500

    updated font size

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Button = ({ btn_text, callback }) => {
   return (
     <button
-      className="text-white bg-new-game-light-blue w-full max-w-[18rem]  rounded-lg py-3 font-semibold"
+      className="text-white text-lg bg-new-game-light-blue w-full max-w-[18rem]  rounded-lg py-3 font-semibold"
       onClick={callback}
     >
       {btn_text}
